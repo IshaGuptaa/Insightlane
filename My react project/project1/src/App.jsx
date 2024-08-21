4fe80ad66d121c4a9620871e723545f3a29dc82a
Isha Gupta <ishagupta6367@gmail.com>
2024-08-21 23:49:09 +0530

    First Commit

---
+++ My react project/project1/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css"
+
+const App = () => {
+  return 
+    <div className="app">
+        <form>
+            <FormInput placeholder="First Name"/>
+            <FormInput placeholder="Last Name"/>
+            <FormInput placeholder="Uername"/>
+            <FormInput placeholder="E-mail"/>
+            <FormInput placeholder="password"/>
+            <FormInput placeholder="Phone No."/>
+            <FormInput placeholder="Country"/>
+            <FormInput placeholder="City"/>
+            <FormInput placeholder="Pan no."/>
+            <FormInput placeholder="AAdhar No."/>
+        </form>
+    </div>;
+  
+};
+
+export default App
